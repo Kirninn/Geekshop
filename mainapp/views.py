f35155822b190153b768bc9a80e17d5f35578ce9
Denis <44545518+Kirninn@users.noreply.github.com>
2021-02-22 11:39:26 +0500

    Добавлен оператор if в test_context.html

--- mainapp/views.py
+++ mainapp/views.py
@@ -15,6 +15,10 @@ def test_context(request):
             {'name': 'Худи черного цвета с монограммами adidas Originals', 'price': '6 090,00'},
             {'name': 'Синяя куртка The North Face', 'price': '23 725,00'},
             {'name': 'Коричневый спортивный oversized-топ ASOS DESIGN', 'price': '3 390,00'},
-        ]
+        ],
+        'product_of_promotions': [
+        {'name': 'Черный рюкзак Nike Heritage', 'price': '2 345,00'},
+        {'name': 'Черные туфли на платформе с 3 парами люверсов Dr Martens 1461 Bex', 'price': '3 590,00'}
+        ],
     }
     return render(request, 'mainapp/test_context.html', context)
