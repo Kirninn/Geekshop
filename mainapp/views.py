81b66fcc2a71323ce6706895d80c16807db88deb
Denis <44545518+Kirninn@users.noreply.github.com>
2021-02-22 11:10:32 +0500

    Добавлен test_context.html

--- mainapp/views.py
+++ mainapp/views.py
@@ -5,3 +5,11 @@ def index(request):
 
 def products(request):
     return render(request, 'mainapp/products.html')
+
+def test_context(request):
+    context = {
+    'title': 'geekshop',
+    'header': 'Добро пожаловать на сайт!',
+    'username': 'Иван Иванов',
+    }
+    return render(request, 'mainapp/test_context.html', context)
