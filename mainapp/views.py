b204f423d8fb28774cc93f3c33bb7799ff5e66da
Denis <44545518+Kirninn@users.noreply.github.com>
2021-02-22 11:24:05 +0500

    Добавден цикл for в test_context.html

--- mainapp/views.py
+++ mainapp/views.py
@@ -8,8 +8,13 @@ def products(request):
 
 def test_context(request):
     context = {
-    'title': 'geekshop',
-    'header': 'Добро пожаловать на сайт!',
-    'username': 'Иван Иванов',
+        'title': 'geekshop',
+        'header': 'Добро пожаловать на сайт!',
+        'username': 'Иван Иванов',
+        'products': [
+            {'name': 'Худи черного цвета с монограммами adidas Originals', 'price': '6 090,00'},
+            {'name': 'Синяя куртка The North Face', 'price': '23 725,00'},
+            {'name': 'Коричневый спортивный oversized-топ ASOS DESIGN', 'price': '3 390,00'},
+        ]
     }
     return render(request, 'mainapp/test_context.html', context)
