ebb78cb3af19d2a344ba92c454bb31c86efaff74
Denis <44545518+Kirninn@users.noreply.github.com>
2021-02-22 15:10:56 +0500

    Работа над домашним заданием второго урока.

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 
 def index(request):
-    context = {'title': 'GeekShop'}
+    context = {
+        'title': 'GeekShop',
+        'name_shop': 'GeekShop Store',
+        'start_buy': 'Начать покупки',
+    }
     return render(request, 'mainapp/index.html', context)
 
 def products(request):
-    context = {'title': 'GeekShop - Каталог'}
+    context = {
+        'title': 'GeekShop - Каталог',
+        'offer_sheet': ['Новинки', 'Одежда', 'Обувь','Аксессуары', 'Подарки', 'Специальные предложения'],
+        }
     return render(request, 'mainapp/products.html', context)
