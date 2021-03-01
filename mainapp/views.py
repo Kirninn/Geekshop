a03595715dfb9d137a02c369fae6e023a663a7f9
Denis <44545518+Kirninn@users.noreply.github.com>
2021-03-01 20:59:21 +0500

    Работа над домашним заданием второго урока. Шестое задание.

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,3 +1,8 @@
+import os
+import json
+
+dir = os.path.dirname(__file__)
+
 from django.shortcuts import render
 
 def index(request):
@@ -12,5 +17,8 @@ def products(request):
     context = {
         'title': 'GeekShop - Каталог',
         'offer_sheet': ['Новинки', 'Одежда', 'Обувь','Аксессуары', 'Подарки', 'Специальные предложения'],
+        'products': [],
         }
+    file_path = os.path.join(dir, 'fixtures/products.json')
+    context.update(json.load(open(file_path, encoding = "utf-8")))
     return render(request, 'mainapp/products.html', context)
