d035488cd5c19dca7fdfea0776a4af41e0a9eb02
Denis <44545518+Kirninn@users.noreply.github.com>
2021-03-08 10:43:00 +0500

    Внесены изменения в диспечер URL.

--- mainapp/views.py
+++ mainapp/views.py
@@ -17,12 +17,9 @@ def index(request):
     return render(request, 'mainapp/index.html', context)
 
 def products(request, pk=None):
-    print(pk)
     context = {
         'title': 'GeekShop - Каталог',
         'categories': ProductCategory.objects.all(),
         'products': Product.objects.all(),
         }
-    file_path = os.path.join(dir, 'fixtures/products.json')
-    context.update(json.load(open(file_path, encoding = "utf-8")))
     return render(request, 'mainapp/products.html', context)
