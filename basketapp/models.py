6d9eb8a78a8213d4aba14a74e2601cad1abc08d1
Denis <44545518+Kirninn@users.noreply.github.com>
2021-03-11 01:00:04 +0500

    Код на конец пятого урока.

--- basketapp/models.py
+++ basketapp/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 
-from mainapp.models import Product
 from authapp.models import User
+from mainapp.models import Product
+
 
 class Basket(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -11,3 +12,6 @@ class Basket(models.Model):
 
     def __str__(self):
         return f'Корзина для {self.user.username} | Продукт {self.product.name}'
+
+    def sum(self):
+        return self.quantity * self.product.price
