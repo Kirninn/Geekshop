3f26f9c4cf30e48dc73bdf5de8facf2d37d0bcab
Denis <44545518+Kirninn@users.noreply.github.com>
2021-03-21 15:13:05 +0500

    Добавлен контролер для обновления пользователя.

--- adminapp/views.py
+++ adminapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, HttpResponseRedirect
 from django.urls import reverse
 
 from authapp.models import User
-from adminapp.forms import UserAdminRegistrationForm
+from adminapp.forms import UserAdminRegistrationForm, UserAdminProfileForm
 
 
 def index(request):
@@ -13,18 +13,27 @@ def admin_users(request):
     return render(request, 'adminapp/admin-users-read.html', context)
 
 def admin_users_create(request):
-        if request.method == 'POST':
-            form = UserAdminRegistrationForm(data = request.POST, files = request.FILES)
-            if form.is_valid():
-                form.save()
-                return HttpResponseRedirect(reverse('admin_staff:admin_users'))
-        else:
-            form = UserAdminRegistrationForm()
-        context = {'form': form}
-        return render(request, 'adminapp/admin-users-create.html', context)
+    if request.method == 'POST':
+        form = UserAdminRegistrationForm(data = request.POST, files = request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('admin_staff:admin_users'))
+    else:
+        form = UserAdminRegistrationForm()
+    context = {'form': form}
+    return render(request, 'adminapp/admin-users-create.html', context)
 
-def admin_users_update(request):
-    return render(request, 'adminapp/admin-users-update-delete.html')
+def admin_users_update(request, user_id):
+    user = User.objects.get(id = user_id)
+    if request.method == 'POST':
+        form = UserAdminProfileForm(data=request.POST, instance = request.user, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('admin_staff:admin_users'))
+    else:
+        form = UserAdminProfileForm(instance = user)
+    context = {'form': form, 'user': user,}
+    return render(request, 'adminapp/admin-users-update-delete.html', context)
 
 def admin_users_delete(request):
     pass
