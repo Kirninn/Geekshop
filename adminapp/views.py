307add56f2de3d71ce79d7111451a8c5ccbbd92c
Denis <44545518+Kirninn@users.noreply.github.com>
2021-03-21 14:20:11 +0500

    Создан контроллер для создания пользователя.

--- adminapp/views.py
+++ adminapp/views.py
@@ -1,16 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.urls import reverse
+
+from authapp.models import User
+from adminapp.forms import UserAdminRegistrationForm
+
 
 def index(request):
     return render(request, 'adminapp/index.html')
 
 def admin_users(request):
-    return render(request, 'adminapp/admin-users-read.html')
+    context = {'users': User.objects.all()}
+    return render(request, 'adminapp/admin-users-read.html', context)
 
 def admin_users_create(request):
-    return render(request, 'adminapp/admin_users_create.html')
+        if request.method == 'POST':
+            form = UserAdminRegistrationForm(data = request.POST, files = request.FILES)
+            if form.is_valid():
+                form.save()
+                return HttpResponseRedirect(reverse('admin_staff:admin_users'))
+        else:
+            form = UserAdminRegistrationForm()
+        context = {'form': form}
+        return render(request, 'adminapp/admin-users-create.html', context)
 
 def admin_users_update(request):
-    return render(request, 'adminapp/admin_users_update_delete.html')
+    return render(request, 'adminapp/admin-users-update-delete.html')
 
 def admin_users_delete(request):
     pass
